fca9c0c02bf7e4231ddd2fc8c0f3560498096405
Maksymilian Tym <tymszczur@gmail.com>
2021-12-11 12:07:15 +0100

    Unfinished solution to task 24 cpp

---
+++ 24.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string tekst;
+    cin>>tekst;
+    char zdanie[tekst.length()]
+    string word = "";
+    for (int i; i=0; auto x : tekst; i++)
+    {
+        zdanie[i] = x;
+    }
+
+    for (int i = 0; i < zdanie.length(); i++) {
+        cout << zdanie[i] << endl;
+    }
+}
