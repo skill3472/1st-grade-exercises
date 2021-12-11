a34397a63012fd7ae7e9bf83f543f99411b6a008
skill3472 <tymszczur@gmail.com>
2021-12-11 11:55:03 +0100

    fixed test2

--- 15.py
+++ 15.py
@@ -8,10 +8,10 @@ print('-'*5)
 m2 = int(input())
 print('Jakie dzialanie chcesz wykonac? \n(1) Dodawanie \n(2) Odejmowanie \n(3) Mnozenie \n(4) Dzielenie')
 y = input()
-	
+
 liczba1 = l1/m1
 liczba2 = l2/m2
-			
+
 if(y == '1'): # Dodawanie
     wynik = liczba1 + liczba2
 elif(y == '2'): # Odejmowanie
