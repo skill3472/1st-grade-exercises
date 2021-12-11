9511390385640c06cb6f5c6789423a935629eca8
Maksymilian Tym <tymszczur@gmail.com>
2021-12-11 17:57:03 +0100

    Task 24 done

--- 24.cpp
+++ 24.cpp
@@ -5,16 +5,27 @@ using namespace std;
 
 int main()
 {
-    string tekst;
-    cin>>tekst;
-    char zdanie[tekst.length()]
-    string word = "";
-    for (int i; i=0; auto x : tekst; i++)
-    {
-        zdanie[i] = x;
-    }
+    string tekst = "przykladowy tekst raz dwa trzy parapet";
+    string slowo;
+    int licznikSlow = 0;
+    string zdanie[100];
 
-    for (int i = 0; i < zdanie.length(); i++) {
-        cout << zdanie[i] << endl;
+    for (int i = 0; i < tekst.length(); i++)
+    {
+        if(tekst[i] != ' ')
+        {
+            slowo += tekst[i];
+        }
+        else
+        {
+            licznikSlow++;
+            zdanie[licznikSlow] = slowo;
+            if(slowo[0] == 'p' && slowo.length() % 2 != 0) cout << endl << slowo;
+            slowo = "";
+        }
     }
+    licznikSlow++;
+    zdanie[licznikSlow] = slowo;
+    cout << endl << slowo;
+    slowo = "";
 }
